8c44488262e207c49e0838a67ccad4efe71a99f4
guiazevedo17 <gui.azevedo@ymail.com>
2022-09-21 22:49:22 -0300

    versao 1.0 finalziada, nao responsiva

---
+++ js/index.js
@@ -0,0 +1,15 @@
+function goProfile(){
+    window.location = "profile.html";
+}
+
+function goDelivery(){
+    window.location = "delivery.html";
+}
+
+function goPayment(){
+    window.location = "payment.html";
+}
+
+function goThanks(){
+    window.location = "thanks.html";
+}
